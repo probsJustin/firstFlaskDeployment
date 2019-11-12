06344045524c534ec28893192ade720e646a2f48
Justin Hagerty <justin.hagerty@dynatrace.com>
2019-11-12 13:06:05 -0500

    test

--- __init__.py
+++ __init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello, I love Digital Ocean but not really!"
+    return "Hello, I love Digital test Ocean but not really!"
 if __name__ == "__main__":
     app.run('0.0.0.0')
